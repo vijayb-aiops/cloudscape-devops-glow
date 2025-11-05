ee8dc71e85f9e2471e856f4e109d72305b12dbff
Vijaya Rajeev Bollu <bvijaycloud@gmail.com>
2025-11-05 10:20:54 -0500

    devops portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/cloudscape-devops-glow/" : "/",
+  // base: mode === "production" ? "/cloudscape-devops-glow/" : "/",
+  base: mode === "production" ? "/" : "/",
   server: {
     host: "::",
     port: 8080,
